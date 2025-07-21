73f8c869b8ce70c5491bcacf1d30b7ac3d002b8d
heyylight <xrhavigamerbryt@gmail.com>
2025-07-20 21:48:11 -0300

    Add files via upload

---
+++ ItemModule.lua
@@ -0,0 +1,60 @@
+local Module = {}
+
+local Players = game:GetService("Players")
+local RunService = game:GetService("RunService")
+
+local LocalPlayer = Players.LocalPlayer
+local ProfileData = require(game.ReplicatedStorage.Modules.ProfileData)
+
+local ItemsFolder = Instance.new("Folder")
+ItemsFolder.Name = "Items"
+ItemsFolder.Parent = workspace
+
+local function CloneItem(Item, ItemType)
+	if not ItemsFolder:FindFirstChild(ProfileData.Weapons.Equipped[ItemType]) then
+		local Clone = Item:Clone()
+		Clone.Anchored = true
+		Clone.Name = ProfileData.Weapons.Equipped[ItemType]
+		Clone.Parent = ItemsFolder
+		return Clone
+	else
+		warn("Clone already exists in ItemsFolder!")
+	end
+end
+
+function Module.Start()
+	if not LocalPlayer.Character or not LocalPlayer.Character:FindFirstChild("HumanoidRootPart") then
+		LocalPlayer.CharacterAdded:Wait()
+	end
+
+	local character = LocalPlayer.Character
+	local GunDisplay, KnifeDisplay
+
+	while true do
+		GunDisplay = character:FindFirstChild("DisplayRefGun")
+		KnifeDisplay = character:FindFirstChild("DisplayRefKnife")
+
+		if GunDisplay and KnifeDisplay then
+			print("ItemGrabber Started.")
+			break
+		end
+
+		task.wait(0.5)
+	end
+
+	if GunDisplay:IsA("ObjectValue") then
+		GunDisplay:GetPropertyChangedSignal("Value"):Connect(function()
+			CloneItem(GunDisplay.Value, "Gun")
+			print("Gun changed:", GunDisplay.Value)
+		end)
+	end
+
+	if KnifeDisplay:IsA("ObjectValue") then
+		KnifeDisplay:GetPropertyChangedSignal("Value"):Connect(function()
+			CloneItem(KnifeDisplay.Value, "Knife")
+			print("Knife changed:", KnifeDisplay.Value)
+		end)
+	end
+end
+
+return Module
